f56127d9a7173e90b8d24a65f9302f71052a613d
buckethead1986 <rspeckpfs@gmail.com>
2021-08-03 23:57:20 -0700

    transform controls show up onClick centered on created balls

--- src/test.js
+++ src/test.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import {
   OrbitControls,
   FlyControls,
+  DragControls,
   PointerLockControls,
   TransformControls,
   PerspectiveCamera,
@@ -13,19 +14,63 @@ import {
 
 function Ball(props) {
   const { args = [0.2, 32, 32], color, position } = props;
+  const [hovered, setHovered] = useState(false);
   const [ref] = useSphere(() => ({
     args: 0.2,
     position: position,
     mass: 1
   }));
-  // console.log(props.position, position);
 
+  // return (
+  //   <mesh
+  //     ref={ref}
+  //     onClick={e => {
+  //       setHovered(true);
+  //       e.stopPropagation();
+  //     }}
+  //     onPointerMissed={() => setHovered(false)}
+  //   >
+  //     {hovered ? (
+  //       <TransformControls>
+          // <sphereBufferGeometry args={args} />
+          // <meshStandardMaterial color={color} />
+  //       </TransformControls>
+  //     ) : (
+  //       <mesh>
+  //         <sphereBufferGeometry args={args} />
+  //         <meshStandardMaterial color={color} />
+  //       </mesh>
+  //     )}
+  //   </mesh>
+  // );
+
+  const orbit = useRef()
+  const transform = useRef()
+  // const mode = useControl("mode", { type: "select", items: ["scale", "rotate", "translate"] })
+  // const { nodes, materials } = useLoader(GLTFLoader, "/scene.gltf")
+  useEffect(() => {
+    if (transform.current) {
+      const controls = transform.current
+      // controls.setMode(mode)
+      const callback = event => (orbit.current.enabled = !event.value)
+      controls.addEventListener("dragging-changed", callback)
+      return () => controls.removeEventListener("dragging-changed", callback)
+    }
+  })
   return (
-    <mesh ref={ref}>
-      <sphereBufferGeometry args={args} />
-      <meshStandardMaterial color={color} />
-    </mesh>
-  );
+    <>
+      <TransformControls ref={transform} position={position}>
+
+          <mesh castShadow receiveShadow >
+          <sphereBufferGeometry args={args} />
+          <meshStandardMaterial color={color} />
+          </mesh>
+
+
+      </TransformControls>
+      <OrbitControls ref={orbit}/>
+    </>
+  )
 }
 
 function Plane(props) {
@@ -42,26 +87,27 @@ function Plane(props) {
   );
 }
 
-function Plane2(props) {
-  const [ref] = usePlane(() => ({
-    rotation: [0, 0, 0]
-  }));
-  return (
-    <mesh
-      ref={ref}
-      onClick={e => console.log("clicked", e)}
-      onPointerDown={() => console.log("pointerDown")}
-    >
-      >
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color={props.color} />
-    </mesh>
-  );
-}
+// function Plane2(props) {
+//   const [ref] = usePlane(() => ({
+//     rotation: [0, 0, 0]
+//   }));
+//   return (
+//     <mesh
+//       ref={ref}
+//       onClick={e => console.log("clicked", e)}
+//       onPointerDown={() => console.log("pointerDown")}
+//     >
+//       >
+//       <planeBufferGeometry attach="geometry" args={[100, 100]} />
+//       <meshLambertMaterial attach="material" color={props.color} />
+//     </mesh>
+//   );
+// }
 
 function Dodecahedron() {
   const { viewport } = useThree();
   // viewport = canvas in 3d units (meters)
+  // const [hovered, setHovered] = useState(false);
 
   const ref = useRef();
   useFrame(({ mouse }) => {
@@ -72,12 +118,10 @@ function Dodecahedron() {
   });
 
   return (
-    <TransformControls>
-      <mesh ref={ref} castShadow>
-        <dodecahedronBufferGeometry attach="geometry" />
-        <meshNormalMaterial attach="material" />
-      </mesh>
-    </TransformControls>
+    <mesh ref={ref} castShadow>
+      <dodecahedronBufferGeometry attach="geometry" />
+      <meshNormalMaterial attach="material" />
+    </mesh>
   );
 }
 
@@ -131,7 +175,6 @@ export default function Test() {
   return (
     <Canvas>
       <FlyControls movementSpeed={10} rollSpeed={0.1} dragToLook={true} />
-      <PointerLockControls />
 
       <Stars />
       <ambientLight intensity={0.5} />
