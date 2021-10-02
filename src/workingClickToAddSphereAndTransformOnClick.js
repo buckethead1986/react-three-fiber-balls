c2490351d3ba0c7630022407903033ce36447aa8
buckethead1986 <rspeckpfs@gmail.com>
2021-10-01 23:59:06 -0700

    cleaned up workingClickToAddSphereAndTransform and added transformControls mode change onClick

--- src/workingClickToAddSphereAndTransformOnClick.js
+++ src/workingClickToAddSphereAndTransformOnClick.js
@@ -9,74 +9,59 @@ import {
   PointerLockControls,
   TransformControls,
   PerspectiveCamera,
-  Stars
+  Stars,
+  useControl
 } from "@react-three/drei";
 
 function Ball(props) {
-  const [state, setState] = useState(props);
-  // console.log(state, props);
+  // const [state, setState] = useState(props);
+  const [mode, setMode] = useState("translate");
   const { args = [0.2, 32, 32], color, position } = props;
-  // const [hovered, setHovered] = useState(false);
-  // const [position, setPosition] = useState(props.position);
-  // const [ref] = useSphere(() => ({
-  //   args: 0.2,
-  //   position: position
-  // }));
-
-  const orbit = useRef();
+  // const orbit = useRef();
   const transform = useRef();
 
-  useEffect(
-    () => {
-      setState({ ...state, active: props.active });
-    },
-    [props]
-  );
-  // const mode = useControl("mode", { type: "select", items: ["scale", "rotate", "translate"] })
-  // const { nodes, materials } = useLoader(GLTFLoader, "/scene.gltf")
+  //--------neither needed
+  // useEffect(
+  //   () => {
+  //     setState({ ...state, active: props.active });
+  //   },
+  //   [props]
+  // );
 
-  //orbit.current.enabled = !event.value;
-  // useEffect(e => {
-  //   if (ref.current) {
-  //     const controls = ref.current;
-  //     // controls.setMode(mode)
-  //
-  //     const callback = event => {
-  //       console.log(`a=${ref.current}`, ref.current, `d=${!event.value}`);
-  //       setPosition([
-  //         ref.current.pointEnd.x,
-  //         ref.current.pointEnd.y,
-  //         ref.current.pointEnd.z
-  //       ]);
-  //     };
+  // useEffect(() => {
+  //   if (transform.current) {
+  //     const controls = transform.current;
+  //     // setMode()
+  //     const callback = event => {console.log(orbit.current.enabled, event.value);
+  //       orbit.current.enabled = !event.value}
   //     controls.addEventListener("dragging-changed", callback);
   //     return () => controls.removeEventListener("dragging-changed", callback);
   //   }
   // });
-  // return (
-  //   <>
-  //     <TransformControls ref={transform} position={position}>
-  //
-  //         <mesh castShadow receiveShadow >
-  //         <sphereBufferGeometry args={args} />
-  //         <meshStandardMaterial color={color} />
-  //         </mesh>
-  //
-  //
-  //     </TransformControls>
-  //     <OrbitControls ref={orbit}/>
-  //   </>
-  // )
+  //------------
 
-  //-----------
-  //separate out box logic into a separate component
-  //send onClick ref up to parent component, and base camera/flycontrols/etc off whether or not youre transforming something.
-  // <OrbitControls ref={orbit}/>
-  // onPointerMissed={e => {e.stopPropagation();console.log('missed', e);setHovered(false);props.setCamera(true)}}>
+  //cycle through 'translate', 'scale', and 'rotate'; transform modes.
+  function changeMode() {
+    switch (mode) {
+      case "translate":
+        return setMode("scale");
+      case "scale":
+        return setMode("rotate");
+      case "rotate":
+        return setMode("translate");
+    }
+  }
+  // const mode = useControl("mode", {
+  //   type: "select",
+  //   items: ["scale", "rotate", "translate"]
+  // });
+  // const { nodes, materials } = useLoader(GLTFLoader, "/scene.gltf")
+
   return (
     <TransformControls
       ref={transform}
       position={position}
+      mode={mode}
       enabled={props.active === props.id ? true : false}
       showX={props.active === props.id ? true : false}
       showY={props.active === props.id ? true : false}
@@ -87,7 +72,16 @@ function Ball(props) {
         castShadow
         receiveShadow
         onClick={e => {
-          props.setActive(props.id);
+          if (props.active === props.id) {
+            changeMode();
+            e.stopPropagation();
+          } else {
+            props.setActive(props.id);
+            e.stopPropagation();
+          }
+        }}
+        onPointerMissed={e => {
+          props.active === props.id && props.setActive("");
           e.stopPropagation();
         }}
       >
@@ -96,6 +90,7 @@ function Ball(props) {
       </mesh>
     </TransformControls>
   );
+  // <OrbitControls ref={transform} />
 }
 
 function Plane(props) {
@@ -155,45 +150,11 @@ export default function Test() {
   const [camera, setCamera] = useState(true);
   const colors = ["#173f5f", "#20639b", "#ff4f79", "#C44536", "#ed553b"];
 
-  // function updateBall(ballId, newPosition) {
-  //   console.log(ballId, newPosition)
-  //   // let newBalls = Object.assign({}, balls)
-  //   // newBalls[ballId].position = newPosition
-  //   // setBalls(newBalls)
-  // }
-
-  // function handleCanvasClick(e) {
-  //   if(camera) {
-  //   let ballId = `ball-${Object.keys(balls).length}`
-  //   let newBalls = Object.assign({}, balls)
-  //   const color = colors[getRandomInt(6)];
-  //   newBalls[ballId] = {
-  //     ballId: ballId,
-  //     updateBall: updateBall,
-  //     color: color,
-  //     setCamera: setCamera,
-  //     position: [e.point.x, e.point.y, e.point.z]
-  //   }
-  //   setBalls(newBalls);
-  //   // console.log(newBalls, balls)
-  // }
-  // }
-
   function handleCanvasClick(e) {
-    // console.log(
-    //   // e,
-    //   e.clientX,
-    //   e.clientY,
-    //   e.target.offsetLeft,
-    //   e.target.offsetTop,
-    //   e.target.clientWidth,
-    //   e.target.clientHeight
-    // );
-    let mouseX =
-      (e.clientX - e.target.offsetLeft) / e.target.clientWidth * 2 - 1;
-    let mouseY =
-      -((e.clientY - e.target.offsetTop) / e.target.clientHeight) * 2 + 1;
-    // console.log(e.point, mouseX, mouseY, counter);
+    // let mouseX =
+    //   (e.clientX - e.target.offsetLeft) / e.target.clientWidth * 2 - 1;
+    // let mouseY =
+    //   -((e.clientY - e.target.offsetTop) / e.target.clientHeight) * 2 + 1;
     if (active === "") {
       let newBalls = [...balls];
       const color = colors[getRandomInt(6)];
@@ -237,7 +198,6 @@ export default function Test() {
   return (
     <Canvas>
       <OrbitControls enabled={active === "" ? true : false} />
-
       <Stars />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
